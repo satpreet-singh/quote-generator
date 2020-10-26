58090600b5d5cdecaf7964ff33aa60c82dc17894
Satti <satpreetsinghsethi99@gmail.com>
2020-10-26 15:07:26 +0530

    Fetching a quote

---
+++ script.js
@@ -0,0 +1,18 @@
+// Get Quote From API
+
+async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+  } catch (error) {
+    console.log("Whoops , we got an error , no quote", error);
+  }
+}
+
+// On Load
+
+getQuote();
